fe28e3ff677ddb562a13c82bb0e4019fbb1e88d6
Michael Gatewood <michael@mngatewood.com>
2018-11-29 21:13:15 -0700

    Adds endpoint for merchant favorite customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,15 @@ class Customer < ApplicationRecord
 
   has_many :invoices
 
+  def self.merchant_favorite(merchant_id)
+    Customer.select("customers.*, COUNT(transactions.id) AS sales")
+            .joins(invoices: :transactions)
+            .where(transactions: {result: 'success'})
+            .where(invoices: {merchant_id: merchant_id})
+            .group(:id)
+            .order("sales desc")
+            .limit(1)
+            .first
+  end
+
 end
